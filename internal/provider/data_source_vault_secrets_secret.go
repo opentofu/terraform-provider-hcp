7e583010662af49c6b29ea5743e38fa8f52ded65
Annalise Diroff <adiroff@hashicorp.com>
2023-08-20 14:14:46 -0700

    Clean up

--- internal/provider/data_source_vault_secrets_secret.go
+++ internal/provider/data_source_vault_secrets_secret.go
@@ -101,13 +101,12 @@ func (d *DataSourceVaultSecretsSecret) Read(ctx context.Context, req datasource.
 		ProjectID:      client.Config.ProjectID,
 	}
 
-	secretValue := ""
 	openSecret, err := clients.OpenVaultSecretsAppSecret(ctx, client, loc, data.AppName.ValueString(), data.SecretName.ValueString())
 	if err != nil {
 		resp.Diagnostics.AddError(err.Error(), "Unable to open secret")
 		return
 	}
-	secretValue = openSecret.Version.Value
+	secretValue := openSecret.Version.Value
 
 	data.ID = data.AppName
 	data.SecretValue = types.StringValue(secretValue)
